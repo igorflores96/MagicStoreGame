d25357e53a7e91ac3ef894cbab0e9a20299d92ac
Tiago Gazzola <tiago.gazzola@hotmail.com>
2023-11-17 23:20:22 -0300

    Arrumado detalhe no script de resetar pos do item

--- MagicStoreGame/Assets/Scripts/Item/DroppedItemsManager.cs
+++ MagicStoreGame/Assets/Scripts/Item/DroppedItemsManager.cs
@@ -2,14 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemRespawn : MonoBehaviour
+public class DroppedItemsManager : MonoBehaviour
 {
     private Vector3 originalPosition;
     private Rigidbody rb;
-
-    [Header ("||| Choose only one boolean bellow |||")]
-    public bool isIngredient;
-    public bool isRecipe;
 
     void Start()
     {
@@ -19,8 +15,7 @@ public class ItemRespawn : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (isIngredient) { ResetItemPosition();  }
-        if (isRecipe)     { DestroyItemProcess(); }
+        ResetItemPosition();
     }
 
     void ResetItemPosition()
@@ -28,12 +23,5 @@ public class ItemRespawn : MonoBehaviour
         rb.isKinematic = true; // Desativa o movimento do item
         transform.position = originalPosition; // Redefine a posição para a posição inicial
         rb.isKinematic = false; // Liga novamente o movimento do item
-    }
-
-    void DestroyItemProcess()
-    {
-        //FINALIZAR LOGICA AQUI PARA ITEM PARAR DE SER DESTRUIDO QUANDO O ITEM FOR PEGO DO CHÃO (RESETAR O 'TIMER')
-        //ADICIONAR JUICE DO IOTEM SENDO DESTRUIDO
-        Destroy(gameObject, 10f); // Destruir o objeto após 10 segundos
     }
 }
